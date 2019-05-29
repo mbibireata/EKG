1aba5bb15e3f19bb6a1bb14e1cd7325444b8c9c2
Mihai Bibireata <mihai.bibireata@gmail.com>
2019-05-29 09:59:23 -0700

    raspberry pi io

---
+++ scope.py
@@ -0,0 +1,35 @@
+from gpiozero import MCP3008
+from time import sleep
+import matplotlib.pyplot as plt 
+from matplotlib import animation
+import numpy as np 
+import time 
+
+#mcp = MCP3008(channel=0)
+
+# Set up scope parameters 
+x_min = 0
+x_max = 2
+y_min = -2
+y_max = 2
+
+# Set up figure, axis, and plot element we want to animate 
+fig = plt.figure() 
+ax = plt.axes(xlim=(x_min, x_max), ylim=(y_min, y_max))
+line, = ax.plot([], [], lw=2)
+
+# Initialization function plotting background of each frame 
+def init():
+    line.set_data([], [])
+    return line,
+
+# Animation function called sequentially
+def animate(i):
+    x = None
+    y = mcp.value
+    line.set_data(x, y)
+    return line,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, interval=20, blit=True)
+
+plt.show()
