16d9d61ffa19fcd5998a4d26bfefd217552df623
Mihai Bibireata <mihai.bibireata@gmail.com>
2019-06-03 12:00:18 -0700

    slight modifications to scope sample

--- scope_sample.py
+++ scope_sample.py
@@ -7,7 +7,7 @@ from gpiozero import MCP3008
 mcp = MCP3008(channel=0, differential=True)
 
 class Scope(object):
-    def __init__(self, ax, maxt=5, dt=0.02):
+    def __init__(self, ax, maxt=10., dt=0.02):
         self.ax = ax
         self.dt = dt
         self.maxt = maxt
@@ -21,10 +21,19 @@ class Scope(object):
         self.ax.add_line(self.line)
         self.ax.set_ylim(self.ymin, self.ymax) 
         self.ax.set_xlim(0, self.maxt)
+        self.last_sample_data = []
+        self.last_sample_t = []
+        self.bpm = 0.
+        self.out_str = "Hello World!"
 
+    #TODO?: logic to not reset image but just to shift all data back smoothly
     def update(self, y):
         lastt = self.tdata[-1]
         if lastt > self.tdata[0] + self.maxt:  # reset the arrays
+	    #Save last samples for analysis
+            self.last_sample_data = self.ydata
+            self.last_sample_t = self.tdata
+	    #reset scope image 
             self.tdata = [self.tdata[-1]]
             self.ydata = [self.ydata[-1]]
             self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
@@ -36,11 +45,29 @@ class Scope(object):
         self.line.set_data(self.tdata, self.ydata)
         return self.line,
 
+    def update2(self, y):
+        lastt = self.tdata[-1]
+        if lastt > self.tdata[0] + self.maxt:
+            self.last_sample_data = self.ydata
+            self.last_sample_t = self.tdata
+
+            self.tdata = [self.tdata[-1]]
+            self.ydata = [self.ydata[-1]]
+            self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
+            self.ax.figure.canvas.draw()
+	
+        t = self.tdata[-1] + self.dt
+        self.tdata.append(t)
+        self.ydata.append(y)
+        self.line.set_data(self.tdata, self.ydata)
+        return self.line,
+
 
 def emitter():
     'return output from MCP'
     while True:
         yield mcp.value
+        #yield np.random.random_sample()
 
 
 fig, ax = plt.subplots()
@@ -49,5 +76,5 @@ scope = Scope(ax)
 # pass a generator in "emitter" to produce data for the update func
 ani = animation.FuncAnimation(fig, scope.update, emitter, interval=3,
                               blit=True)
-
+plt.title(scope.out_str)
 plt.show()
